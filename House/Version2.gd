941b0e8503a6bf8d6d54ec04840ec984ffc5213f
Liuhao Wu <liuhaowu@ucsb.edu>
2021-09-08 13:27:35 -0700

    instance backpack to house and manually move nodes in rug scene to house

--- House/Version2.gd
+++ House/Version2.gd
@@ -3,9 +3,18 @@ extends Node2D
 onready var button = get_node("Panel/Button")
 onready var text = get_node("Panel/RichTextLabel")
 onready var panel = get_node("Panel")
+onready var backpack = get_node("Backpack")
 
 func _on_Button_button_up():
 	button.hide()
 	text.hide()
 	panel.hide()
 	
+
+
+func _on_Button_pressed():
+	backpack.visible = true
+
+
+func _on_Close_Icon_pressed():
+	backpack.visible = false
--- Props 2/Backpack.gd
+++ Props 2/Backpack.gd
@@ -4,8 +4,5 @@ onready var Close = get_node("Background/Close_Icon")
 onready var Bg_label = get_node("Background/Label")
 onready var Backpack = get_parent()
 
-func _on_Close_Icon_button_up():
-	Backpack.visible = false
-
 func _ready():
 	Bg_label.text = "UserName's \nBackpack"
