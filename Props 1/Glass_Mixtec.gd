9257fd538452714bc5380127fd6c37f31e10fb0b
Yanru-Liu <89122795+Yanru-Liu@users.noreply.github.com>
2021-09-09 00:43:21 +0800

    Add files via upload

---
+++ Props 1/Glass_Mixtec.gd
@@ -0,0 +1,18 @@
+extends Popup
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+
+func _on_Glass_Img_pressed():
+	popup()
+	
